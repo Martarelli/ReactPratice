f99c6fd9dde1e533473e44378e14fa1feb081a5a
Martarelli <renanmartarelli@gmail.com>
2024-05-02 16:45:52 -0300

    feat: initial login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
+import logo from './assets/logo.png'
+import './App.css'
 export function App() {
 
   return (
-  
-    <h1>HELLO WORLD</h1>
-  
+    <div className="container">
+      <header className="header">
+        <img src={logo} alt="logo" />
+        <span>Faça seu Login :)</span>
+      </header>
+      <form>
+        <div className="inputContainer">
+          <label htmlFor="email">Usuário</label>
+          <input 
+            type="text" 
+            name="email" 
+            id="email" 
+            placeholder="Digite seu usuário..."/>
+        </div>
+
+        <div className="inputContainer">
+          <label htmlFor="password">Password</label>
+          <input 
+            type="password" 
+            name="password" 
+            id="password" 
+            placeholder="Digite seu Password..."/>
+        </div>
+
+        <a href="">Esqueceu sua senha?</a>
+
+        <button className="button">
+          Se Conectar
+        </button>
+
+        <div className="footer">
+          <p>Não tem usuário?</p>
+          <a href="">Criar Usuário</a>
+        </div>
+      </form>
+    </div>
   )
 }
